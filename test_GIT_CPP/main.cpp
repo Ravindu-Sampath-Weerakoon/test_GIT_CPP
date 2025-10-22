0cea71c559a4628b7abc7f79553d86e610bcc5ce
RAVINDU WEERAKOON <w.m.t.ravindusampath@gmail.com>
2025-10-22 20:08:20 +0530

    user input

--- test_GIT_CPP/main.cpp
+++ test_GIT_CPP/main.cpp
@@ -2,5 +2,12 @@
 #include <iostream>
 int main() {
 	std::cout << "Hello, World!" << std::endl;
+	
+	//get user input
+	std::string name;
+	std::cout << "Enter your name: ";
+	std::cin >> name;
+	std::cout << "Hello, " << name << "!" << std::endl;
+
 	return 0;
 }
